74891d33b84c705a267746bf2edd450cdce0642a
Nikolay Tverezovsky <niktwer@hotmail.com>
2019-09-20 16:56:37 +0300

    HP 20.09

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,19 @@
 var express = require('express');
 var router = express.Router();
 
+var getData = function () {
+    var data = {
+        'item1': 'jpg',
+        'item2': 'cactus-77.jpg',
+        'item3': 'actus-78.jpg'
+    }
+    return data;
+}
+
+
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express', "data": getData()  });
 });
 
 module.exports = router;
